801c078971a5cb8d615d75e893b227793365778e
Yu Kobayashi <yuu.meiji.gkb@gmail.com>
2021-02-04 14:48:44 +0900

    add torchvision models to CIFAR10 experiment

--- experiment/cifar10.py
+++ experiment/cifar10.py
@@ -4,8 +4,10 @@ import torch
 from torch.nn import Module
 from torch.nn import CrossEntropyLoss
 from torch.optim.optimizer import Optimizer
-from torch.utils.data import DataLoader, Dataset
+from torch.utils import data
 from torchvision.datasets import CIFAR10
+from torchvision.models.resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from torchvision.models.densenet import densenet121, densenet161, densenet169, densenet201
 from torchvision.transforms import ToTensor
 
 from experiment.base import BaseExperiment, LossNaError, ResultDict
@@ -20,6 +22,16 @@ MODEL_DICT: Dict[str, Callable] = dict(
     ResNet56=resnet56,
     ResNet110=resnet110,
     DenseNetBC24=densenet_bc,
+
+    ResNet18=resnet18,
+    ResNet34=resnet34,
+    ResNet50=resnet50,
+    ResNet101=resnet101,
+    ResNet152=resnet152,
+    DenseNet121=densenet121,
+    DenseNet161=densenet161,
+    DenseNet169=densenet169,
+    DenseNet201=densenet201,
 )
 
 
@@ -27,7 +39,7 @@ class ExperimentCIFAR10(BaseExperiment):
     def __init__(self, dataset_name='CIFAR10', **kwargs) -> None:
         super(ExperimentCIFAR10, self).__init__(dataset_name=dataset_name, **kwargs)
 
-    def prepare_data(self, train: bool, **kwargs) -> Dataset:
+    def prepare_data(self, train: bool, **kwargs) -> data.Dataset:
         return CIFAR10(root=self.data_dir, train=train, download=True, transform=ToTensor(), **kwargs)
 
     def prepare_model(self, model_name: Optional[str], **kwargs) -> Module:
@@ -36,7 +48,7 @@ class ExperimentCIFAR10(BaseExperiment):
         else:
             raise ValueError(f'Invalid model name: {model_name}')
 
-    def epoch_train(self, net: Module, optimizer: Optimizer, train_loader: DataLoader,
+    def epoch_train(self, net: Module, optimizer: Optimizer, train_loader: data.DataLoader,
                     **kwargs) -> Tuple[Module, ResultDict]:
         running_loss = 0.0
         i = 0
@@ -65,7 +77,7 @@ class ExperimentCIFAR10(BaseExperiment):
             i += 1
         return net, dict(train_loss=running_loss / i, train_accuracy=correct / total)
 
-    def epoch_validate(self, net: Module, test_loader: DataLoader, **kwargs) -> ResultDict:
+    def epoch_validate(self, net: Module, test_loader: data.DataLoader, **kwargs) -> ResultDict:
         running_loss = 0.0
         i = 0
         total = 0
